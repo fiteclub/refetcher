5c37244913f5811b3e43539f6587e591a79a89fa
Matt Fite <mattf255@yahoo.com>
2020-08-18 16:33:10 -0400

    Refactor and refine, conformance to rubocop issues.

--- get_test_data.rb
+++ get_test_data.rb
@@ -6,13 +6,14 @@ require 'nokogiri'
 # Gets example data to experiment with
 
 url = 'http://lancasterpa.devnetwedge.com/parcel/view/5708080100000/2020'
-page = Nokogiri::HTML(open(url))
-page_xml = Nokogiri::XML(open(url))
+# url = 'http://lancasterpa.devnetwedge.com/parcel/view/5708080100000/2020'
+page = Nokogiri::HTML(URI.open(url))
+page_xml = Nokogiri::XML(URI.open(url))
 
 File.open('example.xml', 'w') do |fo|
-    fo.puts page_xml
+  fo.puts page_xml
 end
 
 File.open('example.html', 'w') do |fo|
-    fo.puts page
+  fo.puts page
 end
--- txr.rb
+++ txr.rb
@@ -3,50 +3,44 @@ require 'rubygems'
 require 'open-uri'
 require 'nokogiri'
 
-#testing
-require 'pry'
-require 'jazz_fingers'
-
 # Fetch Real Estate information from public records
-## Copies/uses/borrows heavily from 
+## Copies/uses/borrows/STEALS heavily from
 ## https://stackoverflow.com/questions/820066/nokogiri-select-content-between-element-a-and-b
-# More specifically, fetch html between two <a> refs, 
-# because the records site does not have convenient parent nodes
+# More specifically, fetch html between two <a> refs.
+# The records site does not have usable parent nodes, so grab the html between two css tags.
 
-# Legacy code that I'm keeping, because reasons
+# Legacy code that I'm keeping, because reasons?
 # url = 'http://lancasterpa.devnetwedge.com/parcel/view/0205241900000/2020'
 # page = Nokogiri::HTML(open(url))
 # page = Nokogiri::HTML(File.open("/test/example.html"))
 # page_xml = Nokogiri::XML(open(url))
 # page_xml = Nokogiri::XML(File.read("/test/example.xml"))
 
-# to do : pass this from global variable
-# hard coded for now
-tax_id = "0205241900000"
-tax_year = "2020"
-
-# not used right now -- this is the address to scrape/parse later
-url = "http://lancasterpa.devnetwedge.com/parcel/view/#{tax_id}/#{tax_year}"
-
-target = File.open('example.html')
-page = Nokogiri::XML(File.read(target))
-get_start = page.at_css("a#SalesHistory")
-get_stop = page.at_css("a#BillingCollection")
-
-page_simple = Nokogiri::HTML(File.read(target))
-get_start_simple = page.at_css("a#SalesHistory").to_html
-get_stop_simple = page.at_css("a#BillingCollection").to_html
-
 # IDK why this actually works without passing it the page variable
-def get_portion(start, stop)
-  start == stop ? [start] : [start, *get_portion(start.next, stop)]
+# all credit to Magnus Holm
+def slice_of_css(start, stop)
+  start == stop ? [start] : [start, *slice_of_css(start.next, stop)]
 end
 
-# different technique to get the section of html we need
-#sale_history (NO S!) outputs an array, so must be joined to parse or something idk it just works this way
-sale_history = get_portion(get_start, get_stop)
-puts sale_history
-print sale_history.join().split("\n").map{|ele| ele.strip}
+def txr(tax_id, tax_year)
+  url = "http://lancasterpa.devnetwedge.com/parcel/view/#{tax_id}/#{tax_year}"
+  page = Nokogiri::XML(URI.open(url))
+  puts 
+  puts url
 
+  # Uncomment this block for testing/development with a static page
+  # target = File.open('example.html')
+  # page = Nokogiri::XML(File.read(target))
 
+  page_start = page.at_css("a#SalesHistory")
+  page_stop = page.at_css("a#BillingCollection")
 
+  # sale_history outputs an array of random and garbled html, so must be joined and cleaned
+  sale_history = slice_of_css(page_start, page_stop).join().split("\n").map{ |ele| ele.strip }.reject{ |ele| ele.empty? }
+
+  # Return the latest deed number
+  # Refactor! This is very fragile!
+  print "Last deed#: "
+  puts sale_history[9]
+  sale_history[9]
+end
